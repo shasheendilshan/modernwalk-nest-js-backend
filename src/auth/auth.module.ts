586240d7203832a2fac78451a817b5f676155295
unknown <shasheen@i-threesixty.com>
2022-08-31 14:23:34 +0530

    Add JWT generate and send it as access token when user log in

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -7,11 +7,21 @@ import { UserService } from './../user/user.service';
 import { TenantService } from './../tenant/tenant.service';
 import { User } from './../user/entity/user.entity';
 import { Tenant } from './../tenant/entity/tenant.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User]),
     TypeOrmModule.forFeature([Tenant]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '600s' },
+      }),
+    }),
   ],
   controllers: [AuthController],
   providers: [AuthService, UserService, TenantService],
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,25 +3,39 @@ import { LoginUserDto } from './dto/login-user.dto';
 import { UserService } from './../user/user.service';
 import { CreateUserDto } from '../user/dto/create-user.dto';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   register(createUserDto: CreateUserDto) {
     return this.userService.addUser(createUserDto);
   }
 
   async login(loginAuthDto: LoginUserDto) {
-    const allUsers = await this.userService.getAllUsers();
+    const allUsers = await this.userService.getAllUsersForTenant(
+      loginAuthDto.tenantId,
+    );
     const user = allUsers.find(
       (user) =>
         user.email === loginAuthDto.email &&
         user.tenantId === loginAuthDto.tenantId,
     );
+
     if (!user) throw new UnauthorizedException('Credentials incorrect');
     if (!(await bcrypt.compare(loginAuthDto.password, user.password)))
       throw new UnauthorizedException('Credentials incorrect');
-    return user;
+
+    const { password, tenant, ...userDetails } = user;
+
+    const jwt = await this.jwtService.signAsync({ userDetails });
+    const response = {
+      access_token: jwt,
+    };
+    return response;
   }
 }
